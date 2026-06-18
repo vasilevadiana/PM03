23954472073e5e604dd8b1a541f9c889de9c6fa2
Diana Vasileva <dvasilev@1-A-25.42heilbronn.de>
2026-06-18 13:41:15 +0200

    ex0

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def command_quest() -> None:
+    print("=== Command Quest ===")
+    print("Program name:", sys.argv[0])
+    if (len(sys.argv) == 1):
+        print("No arguments provided!")
+    else:
+        print("Arguments received:", len(sys.argv) - 1)
+        i: int = 0
+        while i < len(sys.argv):
+            print(f"Argument {i}: {sys.argv[i]}")
+            i += 1
+    print("Total arguments:", len(sys.argv))
+
+
+if __name__ == "__main__":
+    command_quest()
