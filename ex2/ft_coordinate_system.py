e5c5fe3ac33fa11ab50ab3d017bf30add3e4cdc2
Diana Vasileva <dvasilev@1-A-21.42heilbronn.de>
2026-06-19 10:08:49 +0200

    added ex2

---
+++ ex2/ft_coordinate_system.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import math
+
+
+def get_player_pos():
+    try:
+        x, y, z = input("Enter new coordinates as floats in format 'x,y,z': ").split(',')
+        coordinates = (float(x), float(y), float(z))
+    except ValueError:
+        print("Invalid syntax")
+        return get_player_pos()
+    return coordinates
+
+def coordinate_system() -> None:
+    print("=== Game Coordinate System ===\n")
+
+    print("Get a first set of coordinates")
+    dot1 = get_player_pos()
+    print("Got a first tuple:", dot1)
+    print(f"It includes: X={dot1[0]}, Y={dot1[1]}, Z={dot1[2]}")
+    distance1 = math.sqrt((dot1[0])**2 + (dot1[1])**2 + (dot1[2])**2)
+    print("Distance to center:", round(distance1, 4))
+    
+    print("Get a second set of coordinates")
+    dot2 = get_player_pos()
+    distance2 = math.sqrt((dot2[0] - dot1[0])**2 + (dot2[1] - dot1[1])**2 + (dot2[2] - dot1[2])**2)
+    print("Distance between the 2 sets of coordinates:", round(distance2, 4))
+
+
+if __name__ == "__main__":
+    coordinate_system()
