a4ed6132c34f01cb41237e595f43eaa9c62f0329
Diana Vasileva <dvasilev@1-A-21.42heilbronn.de>
2026-06-24 17:11:21 +0200

    upd ex1 output format and error type

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -6,17 +6,19 @@ import sys
 def score_analytics() -> None:
     print("=== Player Score Analytics ===")
 
-    i = 0
+    i = 1
     scores: list[int] = []
     while i <= len(sys.argv[1:]):
         try:
             scores.append(int(sys.argv[i]))
-        except TypeError:
+        except ValueError:
             print("Invalid parameter:", sys.argv[i])
         i += 1
     if len(scores) == 0:
-        print("""No scores provided.
-        Usage: python3 ft_score_analytics.py <score1> <score2> ...""")
+        print(
+            "No scores provided. Usage: python3 ft_score_analytics.py "
+            "<score1> <score2> ..."
+        )
     else:
         print("Scores processed:", scores)
         print("Total players:", len(scores))
