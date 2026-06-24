a461fe1692d2e51cc528628f235369538428002a
Diana Vasileva <dvasilev@1-A-25.42heilbronn.de>
2026-06-24 11:14:08 +0200

    upd ex5 deleted global variables

--- ex5/ft_data_stream.py
+++ ex5/ft_data_stream.py
@@ -4,20 +4,13 @@ import random
 from typing import Generator, Tuple, List
 
 
-PLAYERS = ["alice", "bob", "charlie", "dylan"]
-ACTIONS = ["run", "eat", "sleep", "grab",
-           "move", "swim", "climb", "use", "release"]
-
-
-def gen_event() -> Generator[Tuple[str, str], None, None]:
-    """Endless generator yielding (player, action) tuples."""
+def gen_event(players: list[str], actions: list[str]) -> Generator[Tuple[str, str], None, None]:
     while True:
-        yield (random.choice(PLAYERS), random.choice(ACTIONS))
+        yield (random.choice(players), random.choice(actions))
 
 
 def consume_event(event_list: List[Tuple[str, str]]) \
         -> Generator[Tuple[str, str], None, None]:
-    """Yield and remove a random element from `event_list` until empty."""
     while event_list:
         idx = random.randrange(len(event_list))
         yield event_list.pop(idx)
@@ -26,7 +19,11 @@ def consume_event(event_list: List[Tuple[str, str]]) \
 if __name__ == "__main__":
     print("=== Game Data Stream Processor ===\n")
 
-    gen = gen_event()
+    players = ["alice", "bob", "charlie", "dylan"]
+    actions = ["run", "eat", "sleep", "grab",
+               "move", "swim", "climb", "use", "release"]
+
+    gen = gen_event(players, actions)
 
     for i in range(1000):
         name, action = next(gen)
